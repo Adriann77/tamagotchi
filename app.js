e14f7221d0dfc7dacebf322ca6a589399cfb7f30
Adrian <klimasa98@gmail.com>
2023-11-19 19:09:11 -0500

    implemented tamagotchiStates

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 import Game from './js/game.js';
-
+import TamagotchiStates from './js/modules/tamagotchiStates.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+	const currentState = document.querySelector('.rectangle')
 	const game = new Game();
+	const tamaStates = new TamagotchiStates(currentState);
 
 	// Start game
 	game.start({
 		healthElement: '.health__value',
 		hungerElement: '.hunger__value',
 		energyElement: '.sleep__value',
-		funElement: '.fun__value'
+		funElement: '.fun__value',
 	});
+
+	tamaStates.getCurrentStatus();
 });
