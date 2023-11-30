60382d9a62118255a974365f1b3e7abb2094dca1
Adrian <klimasa98@gmail.com>
2023-11-30 09:07:45 +0100

    change values to default 10

--- js/modules/tamagotchi.js
+++ js/modules/tamagotchi.js
@@ -5,8 +5,8 @@ const restartBtn = document.querySelector('.restart');
 export default class Tamagotchi {
 	constructor() {
 		this.state = 'happy';
-		this.health = { value: 1, importance: 1 };
-		this.hunger = { value: 1, importance: 3 };
+		this.health = { value: 10, importance: 1 };
+		this.hunger = { value: 10, importance: 3 };
 		this.energy = { value: 10, importance: 2 };
 		this.fun = { value: 10, importance: 4 };
 
